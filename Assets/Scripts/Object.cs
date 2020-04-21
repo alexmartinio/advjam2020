5085d9e33f0d11c9fa140647c54781dee6f694f6
tompy-dot <52320597+tompy-dot@users.noreply.github.com>
2020-04-21 22:07:26 +0100

    Added click script
    * Clicks are registered globally, there is a click manager
    * Each interactive object would have it's own script to execute logic
    * To run commands for an object if it's clicked it the logic needs to be places in the  public void Execute() function

---
+++ Assets/Scripts/Object.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    SpriteRenderer spriteRender;
+    bool change_colour = true;
+    private void Start()
+    {
+        spriteRender = gameObject.GetComponent<SpriteRenderer>();
+    }
+    public void Execute()
+    {
+        if (change_colour)
+        {
+            spriteRender.color = new Color(1, 0, 0, 1);
+            change_colour = false;
+        }
+        else
+        {
+            spriteRender.color = new Color(1, 1, 1, 1);
+            change_colour = true;
+        }
+    }
+}
