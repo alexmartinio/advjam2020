367c5c368f9cf3233ab9bc076a8cedcf6affa05c
tompy-dot <52320597+tompy-dot@users.noreply.github.com>
2020-05-02 13:19:10 +0100

    Dialogue setup

---
+++ Assets/Scripts/AnimationSwitch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSwitch : MonoBehaviour
+{
+    [SerializeField] GameObject objectToActivate;
+    // Start is called before the first frame update
+
+    Animator anim;
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    public void AnimationFinished()
+    {
+        objectToActivate.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
