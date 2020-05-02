ccd4ac1a2cccc44541d71e5e7d4aef028a440b8b
alexmartinio <10453095+alexmartinio@users.noreply.github.com>
2020-05-02 23:35:50 +0100

    attempt to fix dialogue box and start/end scene

---
+++ Assets/Scripts/CreditsFontSize.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CreditsFontSize : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject headingTemplate = GameObject.Find("Headingg");
+
+        GetComponent<TMP_Text>().fontSize = headingTemplate.GetComponent<TMP_Text>().fontSize;
+        Debug.Log(GetComponent<TMP_Text>().fontSize);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -7,15 +7,20 @@ using UnityEngine.EventSystems;
 public class DialogueManager : MonoBehaviour
 {
     private Queue<string> sentences;
-
+    private Vector3 dialoguePosition;
     public Animator animator;
+    public GameObject dialogueBox;
     public TMP_Text nameText;
     public TMP_Text dialogueText;
     public bool dialogueFinished = false;
 
+
     void Start()
     {
         sentences = new Queue<string>();
+
+        dialogueBox = GameObject.Find("DialogueBox");
+        dialoguePosition = dialogueBox.gameObject.transform.position;
     }
 
     void Update()
@@ -28,12 +33,21 @@ public class DialogueManager : MonoBehaviour
 
     public void StartDialogue(Dialogue dialogue)
     {
-        EventSystem.current.SetSelectedGameObject(this.gameObject); 
+        // Set focus on dialogue box
+        EventSystem.current.SetSelectedGameObject(this.gameObject);
+
+        // Move dialogue box into position
+        dialoguePosition.y = 75f;
+        dialogueBox.transform.position = dialoguePosition;
+
+        // Activate animation
         animator.SetBool("IsOpen", true);
+        
+        // Set up dialogue text
         nameText.text = dialogue.name;
         sentences.Clear();
 
-        foreach(string sentence in dialogue.sentences)
+        foreach (string sentence in dialogue.sentences)
         {
             sentences.Enqueue(sentence);
         }
@@ -59,7 +73,7 @@ public class DialogueManager : MonoBehaviour
     IEnumerator TypeSentence(string sentence)
     {
         dialogueText.text = "";
-        foreach(char character in sentence.ToCharArray())
+        foreach (char character in sentence.ToCharArray())
         {
             dialogueText.text += character;
             yield return null;
