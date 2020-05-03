5995119428dea5e9bc5858f57b56f4b663542a76
alexmartinio <10453095+alexmartinio@users.noreply.github.com>
2020-05-03 17:34:50 +0100

    Add end screen

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void Start()
+    {
+
+        GameObject CrossFade = GameObject.Find("CrossFade");
+        CrossFade.SetActive(true);
+        CrossFade.GetComponent<Canvas>().enabled = true;
+
+        if (Application.platform != RuntimePlatform.WebGLPlayer)
+        {
+            GameObject ExitButton = GameObject.Find("ExitButton");
+            ExitButton.SetActive(false);
+        }
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
